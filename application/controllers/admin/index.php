41fad91348156e1c15e8d1fc49b0d910ad90ef79
Vitaly Serov <serovvitaly@gmail.com>
2013-02-02 17:45:34 +0400

    сделана админка

---
+++ application/controllers/admin/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Admin_Index_Controller extends Base_Controller
+{
+    
+    public function action_index()
+    {
+        return View::make('admin.index');
+    }
+    
+}
--- application/routes.php
+++ application/routes.php
@@ -32,10 +32,8 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('home.index');
-});
+
+Route::controller(Controller::detect());
 
 /*
 |--------------------------------------------------------------------------
