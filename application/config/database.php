c20316244d4cc1d2520b57461b0757809ae923e3
Vitaliy Serov <serovvitaly@gmail.com>
2013-02-04 08:38:23 +0400

    задан пароль БД

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ return array(
 			'host'     => '127.0.0.1',
 			'database' => 'sonet',
 			'username' => 'root',
-			'password' => '',
+			'password' => '123456',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 		),
